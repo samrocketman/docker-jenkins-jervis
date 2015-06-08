8a0411410148f62f8c0e2a1d0770b6cfbae99c3a
Sam Gleske <sam.mxracer@gmail.com>
2015-06-08 00:18:19 -0400

    Better container organization and rvm support

--- jervis-docker-jvm/ubuntu-build/jenkins/install.sh
+++ jervis-docker-jvm/ubuntu-build/jenkins/install.sh
@@ -3,9 +3,6 @@ export PS4='$ '
 echo "Setting up Jenkins client info"
 set -x
 
-#jenkins client requires java
-apt-get install --no-install-recommends openjdk-7-jdk
-
 #add jenkins user with sudo rights
 adduser --quiet jenkins
 echo "jenkins:jenkins" | chpasswd
--- jervis-docker-jvm/ubuntu-build/jvm-packages/install.sh
+++ jervis-docker-jvm/ubuntu-build/jvm-packages/install.sh
@@ -3,4 +3,8 @@ export PS4='$ '
 echo "Setting up JVM packages"
 set -x
 
-apt-get install groovy gradle maven ant scala
+apt-get install groovy gradle maven ant scala openjdk-7-jdk openjdk-6-jdk
+mkdir ~jenkins/java
+chown jenkins. ~jenkins/java
+ln -s /usr/lib/jvm/java-6-openjdk-amd64 ~jenkins/java/openjdk6
+ln -s /usr/lib/jvm/java-7-openjdk-amd64 ~jenkins/java/openjdk7
