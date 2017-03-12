bc969da8f9a5d3349ffc52e76f242edbcdd9d767
Sam Gleske <sam.mxracer@gmail.com>
2017-03-12 06:32:28 -0700

    Add missing CA certificates

--- jervis-docker-jvm/ubuntu-build/jvm-packages/install.sh
+++ jervis-docker-jvm/ubuntu-build/jvm-packages/install.sh
@@ -3,7 +3,8 @@ export PS4='$ '
 echo "Setting up JVM packages"
 set -x
 
-apt-get install groovy gradle maven ant scala openjdk-8-jdk openjdk-9-jdk
+apt-get install groovy gradle maven ant scala openjdk-8-jdk openjdk-9-jdk \
+  ca-certificates ca-certificates-java
 mkdir ~jenkins/java
 chown jenkins. ~jenkins/java
 ln -s /usr/lib/jvm/java-8-openjdk-amd64 ~jenkins/java/openjdk8
