c3ded8f4e284b93d6ee7c6f14829c84bd00ca448
Sam Gleske <sam.mxracer@gmail.com>
2018-02-06 20:20:54 -0800

    Update rvm installation and save keys

--- jervis-docker-jvm/ubuntu-build/rvm-packages/install.sh
+++ jervis-docker-jvm/ubuntu-build/rvm-packages/install.sh
@@ -2,6 +2,6 @@
 export PS4='$ '
 echo "Setting up RVM."
 
-su - jenkins -c 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3'
+su - jenkins -c 'gpg --import - < /build/rvm-packages/keys.asc'
 su - jenkins -c 'bash -s stable < /build/rvm-packages/get.rvm.io'
 su - jenkins -c 'rvm get stable'
