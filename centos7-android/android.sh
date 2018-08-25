fc33de4e6ce804a203e9c11f9972b7d03cb8e831
Sam Gleske <sam.mxracer@gmail.com>
2018-08-25 02:39:41 -0700

    CentOS 7 android image

---
+++ centos7-android/android.sh
@@ -0,0 +1,5 @@
+export ANDROID_HOME=/opt/android-sdk
+export PATH="${ANDROID_HOME}/tools:${PATH}"
+export PATH="${ANDROID_HOME}/tools/bin:${PATH}"
+export PATH="${ANDROID_HOME}/platform-tools:${PATH}"
+export PATH="${ANDROID_HOME}/emulator:${PATH}"
